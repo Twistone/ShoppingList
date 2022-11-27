4df86d40f29fe27586bc79ca60393a72b58ae2bf
Anton <great531@yandex.ru>
2022-11-27 11:51:23 +0500

    Add lesson6

--- app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -26,7 +26,7 @@ class MainViewModel:ViewModel() {
     }
 
     fun changeEnabledState(shopItem: ShopItem){
-        val newItem = shopItem.copy(enabled = !shopItem.enabled)
+        val newItem = shopItem.copy(enabled =! shopItem.enabled)
         editShopItemUseCase.editShopItem(newItem)
     }
 }
